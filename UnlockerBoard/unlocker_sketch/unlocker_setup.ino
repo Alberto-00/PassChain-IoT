3d76b240d55f4902de395861b9c55ab5a9c933ef
Oskar-prg <oskszu2000@gmail.com>
2022-04-21 18:32:58 +0200

    Aggiunta di deep_sleep mode
    Ancora non funziona perfettamente, da revisionare(attualmente commentata in setup).

--- UnlockerBoard/unlocker_sketch/unlocker_setup.ino
+++ UnlockerBoard/unlocker_sketch/unlocker_setup.ino
@@ -1,4 +1,3 @@
-
 void setup(){
  /****************************
   * startDevice_module setup *
@@ -14,9 +13,9 @@ void setup(){
   delay(3500);
 
 
-  /*******************.********
+  /****************************
   * standbyImage_module setup *
-  *********************.*******/
+  *****************************/
   digitalRainAnim.init(&tft);
   digitalRainAnim.setTextColor(4, 188, 200);
   digitalRainAnim.setHeadCharColor(4, 254, 253);
@@ -32,4 +31,15 @@ void setup(){
   tft.setTextColor(tft.color565(3, 211, 216), TFT_BLACK);
   tft.setFreeFont(&Orbitron_Light_24);
   tft.setTextSize(1); //la dimensione va da 1 a 5, di default usa 1
+
+
+  /******************************
+   * function to send the board *
+   *    in deep_sleep mode      *
+   ******************************/
+  /*  NOT WORKING 'A SIGNOR ME NE' MANNAT'   ->   attualmente la scheda mostra il logo e poi va in deep_sleep, da capire perchè
+  //esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);  //Function that wake-up the board after the setted time (is based on the variable TIME_TO_SLEEP that is defined in unlocker_sketch)
+  if(TIME_TO_SLEEP * uS_TO_S_FACTOR > 30)
+    esp_deep_sleep_start(); //The function that actually trigger the deep_sleep mode
+  */
 }
--- UnlockerBoard/unlocker_sketch/unlocker_sketch.ino
+++ UnlockerBoard/unlocker_sketch/unlocker_sketch.ino
@@ -6,6 +6,9 @@
 #define BUTTON1PIN 35
 #define BUTTON2PIN 0
 
+#define uS_TO_S_FACTOR 1000000  /* Fattore di conversione da microsecondi a secondi*/
+#define TIME_TO_SLEEP  30       /* Tempo prima del quale scheda vada in deep_sleep_mode (in secondi)*/
+
 TFT_eSPI tft = TFT_eSPI();
 BleKeyboard bleKeyboard;
 DigitalRainAnim digitalRainAnim = DigitalRainAnim();
