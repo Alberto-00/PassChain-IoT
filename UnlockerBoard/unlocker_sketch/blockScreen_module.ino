2fc8efb91a0a7a426920d5fdc2e90d062ec42421
Oskar-prg <oskszu2000@gmail.com>
2022-04-22 17:02:44 +0200

    Implementazione timeout

--- UnlockerBoard/unlocker_sketch/blockScreen_module.ino
+++ UnlockerBoard/unlocker_sketch/blockScreen_module.ino
@@ -2,19 +2,19 @@
 bool isInactive_device(){
   unsigned long start_time = millis();
   unsigned long stop_time = millis();
-  Serial.println('time: %d', start_time);
-  while((stop_time - start_time) < 30000){
+  Serial.print("Start_time: ");
+  Serial.println(start_time);
+  while(start_time < 30000){
     if(buttonState1 == LOW || buttonState2 == LOW){
+      Serial.println(buttonState1);
       return false; 
     }
-    stop_time = millis();
-      Serial.println(stop_time - start_time);
-
   } 
   return true;
 }
 
 void blockScreen(){
+  //tft.fillScreen(TFT_BLACK);
   digitalRainAnim.loop();
 
   while(buttonState2 == HIGH){
